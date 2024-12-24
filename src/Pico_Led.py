2b0be362e5d39ac4f5d60398ac9de1e1d1ecaf09
Maxwell Mullin <mullinmax@gmail.com>
2024-12-24 17:21:38 -0500

    remove broken pico led func

--- src/Pico_Led.py
+++ src/Pico_Led.py
@@ -22,10 +22,6 @@ def start_fast_blink():
 def start_slow_blink():
     timer.init(freq=1, mode=machine.Timer.PERIODIC, callback=toggle_led)
 
-#TODO not called anywhere but also 1/3 is integer division
-def start_long_short_blink():
-    timer.init(freq=1/3, mode=machine.Timer.PERIODIC, callback=toggle_led)
-
 def off():
     timer.deinit()
     pico_onboard_led.off()
