003f2212b2d4143c4ae1c273fcc4618992f09f35
maxwell mullin <mullinmax@gmail.com>
2025-01-10 20:57:56 -0500

    add original url

--- src/rsa/__init__.py
+++ src/rsa/__init__.py
@@ -1,19 +1,4 @@
-# -*- coding: utf-8 -*-
-# SPDX-FileCopyrightText: 2011 Sybren A. Stüvel <sybren@stuvel.eu>
-#
-# SPDX-License-Identifier: Apache-2.0
-
-"""
-RSA module
-====================================================
-
-Module for calculating large primes, and RSA encryption, decryption, signing
-and verification. Includes generating public and private keys.
-
-**WARNING:** This implementation does not use compression of the cleartext input to
-prevent repetitions, or other common security improvements. Use with care.
-
-"""
+# https://github.com/KipCrossing/micropython_rsa
 
 from rsa.key import PublicKey
 from rsa.pkcs1 import (
