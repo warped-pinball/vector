7d98727c505583de9452af280b9fc77af4b56983
maxwell mullin <mullinmax@gmail.com>
2025-01-11 18:10:48 -0500

    update micropython version

--- dev/build_update.py
+++ dev/build_update.py
@@ -139,7 +139,7 @@ def build_update_file(
         "update_file_format": "1.0",
         "supported_hardware": ["vector_v4", "vector_v5"],
         "supported_software_versions": ["0.3.0"],
-        "micropython_versions": ["1.23.0.preview"],
+        "micropython_versions": ["1.24.1"],
         "version": version
     }
     metadata_line = json.dumps(meta_data, separators=(',', ':'))
