d34f11aeb0d954d3b7dc9803e74a995d63ad1150
maxwell mullin <mullinmax@gmail.com>
2025-01-20 20:47:03 -0500

    removing old update logic

--- src/FileIO.py
+++ src/FileIO.py
@@ -4,11 +4,7 @@ V12/9/2024 support for software download
 """
 import gc
 import json
-import os
 
-import ubinascii
-
-import SharedState as S
 import SPI_DataStore as DataStore
 from logger import logger_instance
 
@@ -21,20 +17,14 @@ def download_scores():
     data.append(
         {
             "FileType": "leaders",
-            "contents": [
-                DataStore.read_record("leaders", i)
-                for i in range(DataStore.memory_map["leaders"]["count"])
-            ],
+            "contents": [DataStore.read_record("leaders", i) for i in range(DataStore.memory_map["leaders"]["count"])],
         }
     )
 
     data.append(
         {
             "FileType": "tournament",
-            "contents": [
-                DataStore.read_record("tournament", i)
-                for i in range(DataStore.memory_map["tournament"]["count"])
-            ],
+            "contents": [DataStore.read_record("tournament", i) for i in range(DataStore.memory_map["tournament"]["count"])],
         }
     )
 
@@ -47,10 +37,7 @@ def download_names(request):
     print("download names - - - ")
     try:
         # Collect player names data
-        names_data = [
-            DataStore.read_record("names", i)
-            for i in range(DataStore.memory_map["names"]["count"])
-        ]
+        names_data = [DataStore.read_record("names", i) for i in range(DataStore.memory_map["names"]["count"])]
 
         response_body = [{"FileType": "names", "contents": names_data}]
         # Prepare the final response
@@ -69,9 +56,7 @@ def download_names(request):
         return json.dumps(
             {
                 "headers": {"Content-Type": "application/json", "Connection": "close"},
-                "body": json.dumps(
-                    {"error": "An error occurred while generating the download."}
-                ),
+                "body": json.dumps({"error": "An error occurred while generating the download."}),
             }
         )
 
@@ -122,124 +107,6 @@ def base64_decode(data):
     return ubinascii.a2b_base64(data)
 
 
-download_results = "File Index List: "
-
-
-# file upload
-def process_incoming_file(request):
-    import gc
-    import json
-
-    import ubinascii
-
-    global download_results
-
-    # Base64 decoding for MicroPython
-    def base64_decode(data):
-        """Decode a Base64 encoded string into bytes."""
-        return ubinascii.a2b_base64(data)
-
-    gc.collect()
-    try:
-        form_data = request.form
-        for key in form_data:
-            value = form_data[key]
-
-        gc.collect()
-
-        data = json.loads(value)
-        file_type = data.get("FileType")
-        contents = data.get("contents")
-        received_checksum = data.get("Checksum")
-        is_binary = data.get("Binary", False)  # default to text if missing
-        append = data.get("append", 1)  # Default to 1 if "append" is missing
-        index = data.get("index", 1)
-
-        # check for match to fram data store first - -
-        if file_type in DataStore.memory_map:
-            Log.log(f"FIO: Datastore file in: {file_type}")
-            for idx, record in enumerate(contents):
-                # print("FIO: ",idx)
-                DataStore.write_record(file_type, record, idx)
-            download_results += f" {file_type}"
-            return "Upload complete"  # **************************************
-
-        Log.log(
-            f"FIO: Load file: {file_type}, I{index} A{append}{' B' if is_binary else ''}"
-        )
-
-        if is_binary:
-            contents = base64_decode(contents)
-            calculated_checksum = crc16(contents)  # Use raw binary data
-        else:
-            calculated_checksum = crc16(contents.encode("utf-8"))  # Encode as UTF-8
-
-        # Verify checksum
-        received_checksum = received_checksum.lower()
-        calculated_checksum = f"{calculated_checksum}".lower()
-
-        if calculated_checksum != received_checksum:
-            Log.log(
-                f"FIO: Checksum fail. Expected {received_checksum}, got {calculated_checksum}"
-            )
-        else:
-            print("FIO: Checksum good:", f"{calculated_checksum}")
-
-        save_path = f"{file_type}"
-        print("FIO: Save Path: ", save_path)
-
-        # Determine file mode based on append value
-        # mode = 'ab' if append > 1 else 'wb' if is_binary else 'a' if append > 1 else 'w'
-        if append > 1:
-            mode = "ab" if is_binary else "a"
-        else:
-            mode = "wb" if is_binary else "w"
-
-        # Save file
-        with open(save_path, mode) as f:
-            f.write(contents if is_binary else contents.encode("utf-8"))
-        print(f"FIO: File {'appended' if append > 1 else 'saved'}: {file_type}")
-
-        gc.collect()
-
-        # If it's RunMe.py (must fit in one chunk!)
-        if file_type == "RunMe.py":
-            print("FIO: Executing RunMe.py")
-            module_name = "RunMe"
-            imported_module = __import__(module_name)
-            try:
-                result = imported_module.go()  # Call the main function
-                download_results += f" RM "
-                if result is not None:
-                    Log.log(f"FIO: RunMe returned {result}")
-                    S.update_load_result = result
-                else:
-                    Log.log("FIO: RunMe executed, no return value")
-            except ImportError as e:
-                print(f"FIO: Error importing module {module_name}: {e}")
-            except Exception as e:
-                print(f"FIO: An error occurred while running {module_name}.go(): {e}")
-        else:
-            download_results += f"{index} "
-
-        return "Upload complete"
-
-    except Exception as e:
-        Log.log(f"Error processing file upload: {e}")
-        S.update_load_result = "FIO: General Fault"
-        return f"An error occurred: {e}"
-
-
-def incoming_file_results(request):
-    global download_results
-    S.update_load_result = download_results
-    download_results = "File Index List: "
-    if S.update_load_result is None:
-        return "Nothing to report"
-    else:
-        return S.update_load_result
-
-
 def set_file_size(path, num_bytes, preserve_to_byte=0):
     # makes a file of a certain size with minimal edits
     # used in updating files
@@ -262,7 +129,7 @@ def set_file_size(path, num_bytes, preserve_to_byte=0):
                 file_size = num_bytes
 
     # catch ENOENT exception
-    except OSError as e:
+    except OSError:
         # if the file does not exist, create it with the correct size
         with open(path, "w") as f:
             print(f"Creating file {path} with {num_bytes} null bytes")
