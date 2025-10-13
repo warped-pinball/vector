4fc581b266d1ab3c1652e04d1063adf2b1568104
Maxwell Mullin <mullinmax@gmail.com>
2025-10-13 15:24:14 -0400

    Update systemConfig.py

--- src/sys11/systemConfig.py
+++ src/sys11/systemConfig.py
@@ -2,7 +2,7 @@ vectorSystem = "sys11"
 updatesURL = "http://software.warpedpinball.com/vector/sys11/latest.json"
 
 # Firmware version for the System11 build
-SystemVersion = "1.4.1"
+SystemVersion = "1.5.0"
 
 
 # System specific scheduled tasks
