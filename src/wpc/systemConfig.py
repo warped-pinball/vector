5deee0841121b52eef12f9d159ac4a6b01141b28
Paul <mullinfarm@gmail.com>
2025-10-25 12:32:43 -0400

    Adding Circus Voltaire config file (#145)
    * json added
    
    * version bump

--- src/wpc/systemConfig.py
+++ src/wpc/systemConfig.py
@@ -2,7 +2,7 @@ vectorSystem = "wpc"
 updatesURL = "http://software.warpedpinball.com/vector/wpc/latest.json"
 
 # Firmware version for the WPC build
-SystemVersion = "1.1.2"
+SystemVersion = "1.1.3"
 
 
 # System specific scheduled tasks
