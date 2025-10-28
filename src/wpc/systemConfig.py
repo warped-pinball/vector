2dcaa38a7101e80ef83c54d48e3473b048da2671
Paul <mullinfarm@gmail.com>
2025-10-27 20:15:22 -0400

    Getaway high Speed II - rom L2 add (#147)
    * getaway rom L2 add
    
    * Update src/wpc/config/Getaway_L2.json
    
    * Update VectorVersion to 1.5.6
    
    ---------
    
    Co-authored-by: Maxwell Mullin <mullinmax@gmail.com>

--- src/wpc/systemConfig.py
+++ src/wpc/systemConfig.py
@@ -2,7 +2,7 @@ vectorSystem = "wpc"
 updatesURL = "http://software.warpedpinball.com/vector/wpc/latest.json"
 
 # Firmware version for the WPC build
-SystemVersion = "1.1.4"
+SystemVersion = "1.1.5"
 
 
 # System specific scheduled tasks
