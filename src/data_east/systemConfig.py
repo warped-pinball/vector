e8baf3e980c43c56104ec8097a9178c9f48d4ba8
Maxwell Mullin <mullinmax@gmail.com>
2025-11-25 12:58:29 -0500

    Version bump (#201)
    * Update VectorVersion to 1.9.0
    
    * Update SystemVersion to 0.3.0
    
    * Update SystemVersion to 1.5.0
    
    * Update SystemVersion to 1.8.0
    
    * Update systemConfig.py
    
    * Update SystemVersion to 1.5.0

--- src/data_east/systemConfig.py
+++ src/data_east/systemConfig.py
@@ -2,7 +2,7 @@ vectorSystem = "data_east"
 updatesURL = "http://software.warpedpinball.com/vector/data_east/latest.json"
 
 # Firmware version for the Data East build
-SystemVersion = "0.2.0"
+SystemVersion = "0.3.0"
 
 
 # System specific scheduled tasks
