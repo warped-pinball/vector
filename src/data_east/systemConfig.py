8dbe6f38ad2d9b83636f56b09cd7d7796e653ab6
Maxwell Mullin <mullinmax@gmail.com>
2025-12-30 12:50:02 -0500

    Version bump (#225)
    * 1.7.0
    
    * Revert firmware version to 1.3.0 in systemConfig.py
    
    * Revert firmware version to 1.7.0 in systemConfig.py
    
    * Revert firmware version to 1.3.0 in systemConfig.py
    
    * update versions, minor

--- src/data_east/systemConfig.py
+++ src/data_east/systemConfig.py
@@ -2,7 +2,7 @@ vectorSystem = "data_east"
 updatesURL = "http://software.warpedpinball.com/vector/data_east/latest.json"
 
 # Firmware version for the Data East build
-SystemVersion = "0.3.1"
+SystemVersion = "0.4.0"
 
 
 # System specific scheduled tasks
--- src/em/SharedState.py
+++ src/em/SharedState.py
@@ -1,5 +1,7 @@
 # Common firmware version shared by all builds
-VectorVersion = "1.3.13"
+VectorVersion = "1.10.0"
+# TODO this should not be duplicated from common/origin.py if we forget to update both places EMs will always think they are out of date
+
 
 # counts game start cycles
 gameCounter = 0
@@ -11,15 +13,10 @@ update_load_result = None
 faults = []
 
 # game data  (specific title data)
-gdata = {
-    "numberOfPlayers": 2,
-    "digitsPerPlayer": 4,
-    "dummy_reels": 0
-}
+gdata = {"numberOfPlayers": 2, "digitsPerPlayer": 4, "dummy_reels": 0}
 
 # game status
 game_status = {}
 
 
 run_learning_game = False
-
