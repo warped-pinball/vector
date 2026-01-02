7f590363bd80771bd35481e1b6d56438816c163a
Paul <mullinfarm@gmail.com>
2026-01-02 15:08:11 -0500

    basic added files working

---
+++ src/common/BoardLED.py
@@ -0,0 +1,133 @@
+# This file is part of the Warped Pinball SYS11Wifi Project.
+# https://creativecommons.org/licenses/by-nc/4.0/
+# This work is licensed under CC BY-NC 4.0
+
+"""
+    Warped Pinball
+    
+    Low level driver for WS2812 combined with single color led legacy support
+"""
+import machine
+try:
+    import rp2
+except:
+    rp2 = None
+
+
+# Color definitions for board LEDs
+RED     = 0x000C00
+GREEN   = 0x0C0000
+GREENDIM = 0x090000
+GREENDIMM = 0x030000
+BLUE    = 0x00000C
+YELLOW  = 0x0C0C00
+CYAN    = 0x000C0C
+MAGENTA = 0x0C000C
+WHITE   = 0x0C0C0C
+BLACK   = 0x000000
+
+
+#WS2812 serial driver with suuport for single color LED on same gpio
+@rp2.asm_pio(
+    sideset_init=rp2.PIO.OUT_LOW,
+    out_shiftdir=rp2.PIO.SHIFT_LEFT,
+    autopull=True,
+    pull_thresh=24,     # 24 bits per pixel (GRB)
+)
+def ws2812():
+    wrap_target()
+
+    label("bitloop")
+    out(x, 1)               .side(0)    [2]        
+    jmp(not_x, "do_zero")   .side(1)    [1]          
+    jmp("bitloop")          .side(1)    [3]         
+    label("do_zero")
+    nop()                   .side(0)    [3]        
+    jmp(not_osre,"bitloop") #loop all data out (24 bits)
+
+    set(x,14)               #delay with output low to set the data  
+    label("after_loop")
+    wait(1,irq,2)
+    jmp(x_dec,"after_loop")
+
+    jmp(not_y,"low_output") #set up the i/o pin output for single color led compatibility
+    nop()   .side(1)
+    label("low_output")
+
+    label("wait")           #hold line high or low for simple LED, wait for data
+    jmp(not_osre,"preplow")     
+    jmp("wait")
+    label("preplow")
+
+    set(x,14)    .side(0)  #give some low time in prep for next bit string
+    label("before_loop")
+    wait(1,irq,2)
+    jmp(x_dec,"before_loop")
+
+    wrap()
+    
+#low speed clock source for delays required in  ws2812()
+@rp2.asm_pio()
+def irq_clock():
+    wrap_target()
+    irq(2)                
+    set(x, 21)            
+    label("delay")
+    jmp(x_dec, "delay")
+    wrap()
+
+
+#main PIO state machine for control of LEDs
+sm_led = None
+
+def startUp():        
+    """
+        must call once at power up - only if running on PICO2
+    """
+    global sm_led
+
+    clk_led = rp2.StateMachine(
+        6,               # state machine index
+        irq_clock,       
+        freq=1_000_000,       
+    )
+    clk_led.active(1)
+
+    sm_led = rp2.StateMachine(
+        7,              # state machine index
+        ws2812,         
+        freq=8_000_000, 
+        sideset_base=machine.Pin(26)
+    )
+    sm_led.active(1)
+
+    print("BrdLED: start")
+
+
+ledState = 0
+lastColor = BLACK
+
+def ledOn():
+    global sm_led,ledState
+    sm_led.exec("set(y, 0)") 
+    ledState=1
+
+def ledOff():
+     global sm_led,ledState
+     sm_led.exec("set(y, 1)") 
+     ledState=0
+
+def ledtoggle():
+    # toggle also auto updates state!
+    global ledState
+    if ledState==0:
+        ledOn()
+    else:
+        ledOff()
+    ledColor(lastColor)
+
+def ledColor(c):
+    global lastColor
+    #change in color updates led on/off status also
+    lastColor = c
+    sm_led.put(c<<8)  
---
+++ src/common/FaultIndicator.py
@@ -0,0 +1,144 @@
+
+# This file is part of the Warped Pinball SYS11Wifi Project.
+# https://creativecommons.org/licenses/by-nc/4.0/
+# This work is licensed under CC BY-NC 4.0
+
+"""
+    Warped Pinball
+    
+    Fault indicator 
+    Supports Ws2812 multi color LED for enhanced fault reporting via BoardLED.py driver
+"""
+
+import time,utime
+import machine
+import SharedState
+import BoardLED as L
+import faults
+try:
+    import rp2
+except:
+    rp2 = None
+
+
+# Map each fault type to a list of colors for LED indication
+#     first in list has priority  
+FAULT_COLOR_SEQUENCES = {
+    "ALL_OK":  [L.GREEN, L.GREENDIM, L.GREEN, L.GREENDIMM],
+    "GEN_FLT": [L.RED, L.RED,L.RED,L.RED,L.RED,L.RED, L.BLACK],
+    "WIFI00":  [L.RED, L.CYAN, L.CYAN, L.CYAN, L.BLACK],
+    "WIFI01":  [L.RED, L.RED, L.BLUE, L.BLACK],
+    "HDWR01":  [L.RED, L.MAGENTA, L.BLACK],
+    "SFTW01":  [L.MAGENTA, L.WHITE, L.BLACK],
+
+}
+
+enableWS2812led = False
+timer = machine.Timer()
+LED_Out = None
+sequence = [L.BLACK, L.BLACK]
+
+
+def isChip2350():
+    """
+        Determine if we are running on 2350 with PIO support for new ws2812 LED
+    """
+    import os
+    chip_id = os.uname().machine
+  
+    if "RP2350" in chip_id and rp2 != None:
+        print("FLT_IND: Pico 2 (RP2350) detected")       
+        return True
+    return False
+
+
+
+def toggleBoardLED():
+    """
+        Legacy interface to led function
+          -works if we are on a PICO1 without BoardLED running
+          -works on PICO2 with BoardLED and only single color LED installed
+          -runs with no effect on PICO2 with only ws2812 led installed
+    """
+    global enableWS2812led
+    if enableWS2812led == 1:
+        #ws2812 driver - control of single color LED
+        L.ledtoggle()
+    else:
+        #old single color led only
+        LED_Out.toggle()
+
+
+def start():
+    """
+        start up fault indicator once
+    """
+    global LED_Out, timer, enableWS2812led
+    enableWS2812led = isChip2350()
+    if enableWS2812led is True:
+        L.startUp()
+        L.ledOff()
+        L.ledColor(L.BLACK)
+        timer.init(period=790, mode=machine.Timer.PERIODIC, callback=_timerCallBack)
+    else:
+        LED_Out = machine.Pin(26, machine.Pin.OUT)
+
+  
+
+def _getNewFaults():
+    global x, sequence
+    """
+        Check SharedState for faults and update LED sequence accordingly
+    """
+    # Extract fault codes from SharedState.faults
+    fault_codes = [f.split(":")[0].split()[0] for f in SharedState.faults]
+
+    # Find the first matching fault code in FAULT_COLOR_SEQUENCES
+    selected_seq = None
+    if fault_codes:
+        for key in FAULT_COLOR_SEQUENCES:
+            if key in fault_codes:
+                selected_seq = FAULT_COLOR_SEQUENCES[key]
+                break
+        if selected_seq is None:
+            selected_seq = FAULT_COLOR_SEQUENCES.get("GEN_FLT")
+    else:
+        selected_seq = FAULT_COLOR_SEQUENCES.get("ALL_OK")
+
+    # Copy the selected color sequence into seq
+    sequence = list(selected_seq)
+
+
+
+index = 0
+def _timerCallBack(_):
+    """
+        timer to drive LED blinky blink - and get new FAULTS periodically
+    """
+    global index,sequence
+    L.ledColor(sequence[index])
+    index = (index + 1) % (len(sequence)) 
+    if index==0:
+        _getNewFaults()
+    
+
+
+
+
+
+#  TEST - - 
+if __name__ == "__main__":
+    start()
+
+    #generic fault
+    faults.raise_fault(faults.WIFI02, f"Invalid wifi credentials ")
+
+    #known fault
+    #faults.raise_fault(faults.WIFI00, f"Invalid wifi credentials ")
+    #faults.raise_fault(faults.WIFI01, f"Invalid wifi credentials ")
+
+    time.sleep(2)
+    L.ledtoggle()
+    time.sleep(5)
+    L.ledtoggle()
+    time.sleep(2)
